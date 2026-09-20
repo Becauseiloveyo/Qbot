65dcde02b281328b2d218d49e7e722cfc262f34e
Becauseiloveyo <1079458933@qq.com>
2026-09-20 20:54:57 +0800

    test: isolate Robolectric tests from application startup

--- android/app/src/test/java/dev/qbot/android/runtime/SendExecutorRoomTest.kt
+++ android/app/src/test/java/dev/qbot/android/runtime/SendExecutorRoomTest.kt
@@ -1,5 +1,6 @@
 package dev.qbot.android.runtime
 
+import android.app.Application
 import android.content.Context
 import androidx.room.Room
 import dev.qbot.android.data.AgentRunRepository
@@ -25,7 +26,7 @@ import org.robolectric.RuntimeEnvironment
 import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
-@Config(sdk = [37])
+@Config(sdk = [37], application = Application::class)
 class SendExecutorRoomTest {
     private lateinit var context: Context
     private lateinit var database: QbotDatabase
