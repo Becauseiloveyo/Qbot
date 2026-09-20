3eb1867430b4ddd090ca4612cdfd41a06d837b33
Becauseiloveyo <1079458933@qq.com>
2026-09-20 14:42:50 +0800

    desktop: expose transport contracts

---
+++ desktop/qbot/transport/__init__.py
@@ -0,0 +1,19 @@
+from .base import (
+    DeliveryLookupResult,
+    IncomingTransportEvent,
+    OutgoingMessage,
+    QQTransport,
+    SendResult,
+    TransportCapability,
+)
+from .mock import MockTransport
+
+__all__ = [
+    "DeliveryLookupResult",
+    "IncomingTransportEvent",
+    "MockTransport",
+    "OutgoingMessage",
+    "QQTransport",
+    "SendResult",
+    "TransportCapability",
+]
